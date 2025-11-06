c5828dc58f55773b23a2699b6575ac1b55c5b6b9
Poonnang Slate <pkaraket73@gmail.com>
2025-11-05 18:45:27 -0600

    added layout components

--- potion-smith/src/App.jsx
+++ potion-smith/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -6,7 +8,8 @@ function App() {
 
   return (
     <>
-      
+      <Header />
+      <Footer />
     </>
   )
 }
---
+++ potion-smith/src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+const NavBar = () => {
+    return (
+       <nav className = "navbar">
+           <div className="navbar-left">
+                <a href="/" className="logo">Potion Smith</a>
+            </div>
+            <div className="navbar-cener">
+               <ul className="">
+
+              
+                <li>
+                    <a href="/recipes">Recipes</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>"
+                </li>
+                <li>
+                    <a href="/subscribe">Subscribe</a>"
+                </li>
+                </ul> 
+            </div>                
+                            
+                            
+                            
+                            
+        </nav>
+      
+    )
+}
