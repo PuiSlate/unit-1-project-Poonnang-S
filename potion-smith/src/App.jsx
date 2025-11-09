bdc1d65e92fdde17d5004645fdced4982fb56280
Poonnang Slate <pkaraket73@gmail.com>
2025-11-09 15:06:47 -0600

    added pages components

--- potion-smith/src/App.jsx
+++ potion-smith/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import SubscribePage from './components/pages/SubscribePage'
+
 
 
 function App() {
@@ -6,7 +8,8 @@ function App() {
 
   return (
     <>
-      
+      <SubscribePage />
+
     </>
   )
 }
---
+++ potion-smith/src/components/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+const HomePage = () => {
+    return (
+        <main>
+            <div className="main-content">
+            <h1>Featured This Week</h1>
+            <a href="https://www.potionsmith.com/recipes/rangers-feral-senses"><img 
+            src="https://drive.google.com/file/d/1DjgR9uxQQITfFPEnC9aT8p4WzKUzNcqe/view?usp=drive_link" 
+            alt="Ranger's Feral Senses"></img>
+            </a>
+            <a href="https://www.potionsmith.com/recipes/hellfire-margarita"><img
+            src="https://drive.google.com/file/d/1HvRa3OH82FFsRxaI3rZzACchQbdfbx7f/view?usp=drive_link"
+            alt="HellFire Margarita"></img>
+            </a>
+            <a href="https://www.potionsmith.com/recipes/bard-song-of-rest"><img
+            src="https://drive.google.com/file/d/11T52tbfSaBldybMSczVbVQMwk2fZX-Gh/view?usp=drive_link"
+            alt="Bard's Song of Rest"></img>       
+            </a>
+
+            </div>
+        </main>
+    )
+}
+
+export default HomePage;
